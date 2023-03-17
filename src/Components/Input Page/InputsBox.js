41e282548023b687ce5daaf37d6ab83c2592abfc
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2023-03-17 20:43:17 -0300

    feat: Create Form with Formik and validation with Yup

--- src/Components/Input Page/InputsBox.js
+++ src/Components/Input Page/InputsBox.js
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { ProductContext } from "../../Context/Products";
 import { HiArrowUturnLeft } from "react-icons/hi2";
-import * as Yup from "yup"
-import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage} from "formik";
 
 export default function InputBox() {
   const navigate = useNavigate();
@@ -19,15 +19,40 @@ export default function InputBox() {
   const [value, setValue] = useState(0);
   const [image, setImage] = useState(0);
   const [category, setCategory] = useState("");
-  const {setLoadingProducts} = useContext(ProductContext)
-  const barCode = (Math.floor(Math.random() * 999999999999))
-  const toDay = dayjs().format('DD/MM/YYYY')
+  const { setLoadingProducts } = useContext(ProductContext);
+  const barCode = Math.floor(Math.random() * 999999999999);
+  const toDay = dayjs().format("DD/MM/YYYY");
+
+  const initialValues = {
+    name: "",
+    description: "",
+    height: "",
+    width: "",
+    length: "",
+    weight: "",
+    value: "",
+    image: "",
+    category: ""
+  };
+
+  const schema = Yup.object().shape({
+    name: Yup.string().min(3, 'Precisa ter no mínimo três letras').required('Esse campo é obrigatório'),
+    description: Yup.string().min(3, 'Precisa ter no mínimo três letras').required('Esse campo é obrigatório'),
+    height: Yup.number().required('Esse campo é obrigatório'),
+    width: Yup.number().required('Esse campo é obrigatório'),
+    length: Yup.number().required('Esse campo é obrigatório'),
+    weight: Yup.number().required('Esse campo é obrigatório'),
+    value: Yup.number().required('Esse campo é obrigatório'),
+    image: Yup.string().min(3, 'Precisa ter no mínimo três letras').required('Esse campo é obrigatório'),
+    category: Yup.string().min(3, 'Precisa ter no mínimo três letras').required('Esse campo é obrigatório'),
+  });
+
   const body = {
     title,
     description,
-      height,
-      width,
-      length,
+    height,
+    width,
+    length,
     weight,
     barCode,
     dateAcquisition: toDay,
@@ -36,104 +61,143 @@ export default function InputBox() {
     category,
   };
 
+  const renderErrorMessage = (message) => (
+    <p style={{ color: 'red' }}>{message}</p>
+  );
+
   function addProduct() {
     axios
       .post("http://localhost:1234/Products", body)
       .then((resp) => {
-        console.log(resp)
-        setLoadingProducts(true)
+        console.log(resp);
+        setLoadingProducts(true);
       })
       .catch((err) => console.log(err));
-      navigate("/")
+    navigate("/");
   }
 
   return (
     <>
       <InputBoxContainer>
-      <HiArrowUturnLeft className="arrowBack" onClick={()=> navigate("/")}/>
+        <HiArrowUturnLeft className="arrowBack" onClick={() => navigate("/")} />
         <Title>Adicione um produto</Title>
-        <form onSubmit={() => addProduct()}>
-          <input
-            placeholder="Nome do produto"
-            required
-            type="text"
-            onChange={(e) => setTitle(e.target.value)}
-          ></input>
-          <input
-            placeholder="Descrição do produto"
-            required
-            type="text"
-            onChange={(e) => setDescription(e.target.value)}
-          ></input>
-          <input
-            placeholder="Altura do produto"
-            required
-            type="number"
-            onChange={(e) => setHeight(e.target.value)}
-          ></input>
-          <input
-            placeholder="Largura do produto"
-            required
-            type="number"
-            onChange={(e) => setWidth(e.target.value)}
-          ></input>
-          <input
-            placeholder="Comprimento do produto"
-            required
-            type="number"
-            onChange={(e) => setLength(e.target.value)}
-          ></input>
-          <input
-            placeholder="Peso do produto"
-            required
-            type="number"
-            onChange={(e) => setWeight(e.target.value)}
-          ></input>
-          <input
-            placeholder="Valor do produto"
-            required
-            type="number"
-            onChange={(e) => setValue(e.target.value)}
-          ></input>
-          <input
-            placeholder="Foto do produto"
-            required
-            type="text"
-            onChange={(e) => setImage(e.target.value)}
-          ></input>
-          <input
-            placeholder="Categoria do produto"
-            required
-            type="text"
-            onChange={(e) => setCategory(e.target.value)}
-          ></input>
-          <button>Adicionar produto</button>
-        </form>
+        <Formik initialValues={initialValues} validationSchema={schema} onSubmit={addProduct}>
+          {(formik) => (
+            <Form>
+              <Field
+                placeholder="Nome do produto"
+                name="name"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setTitle(event.target.value);
+                }}
+              />
+              <ErrorMessage name="name" render={renderErrorMessage}/>
+              <Field
+                placeholder="Descrição do produto"
+                name="description"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setDescription(event.target.value);
+                }}
+              />
+              <ErrorMessage name="description" render={renderErrorMessage}/>
+              <Field
+                placeholder="Altura do produto"
+                name="height"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setHeight(event.target.value);
+                }}
+              />
+              <ErrorMessage name="height" render={renderErrorMessage}/>
+              <Field
+                placeholder="Largura do produto"
+                name="width"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setWidth(event.target.value);
+                }}
+              />
+              <ErrorMessage name="width" render={renderErrorMessage}/>
+              <Field
+                placeholder="Comprimento do produto"
+                name="length"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setLength(event.target.value);
+                }}
+              />
+              <ErrorMessage name="length" render={renderErrorMessage}/>
+              <Field
+                placeholder="Peso do produto"
+                name="weight"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setWeight(event.target.value);
+                }}
+              />
+              <ErrorMessage name="weight" render={renderErrorMessage}/>
+              <Field
+                placeholder="Valor do produto"
+                name="value"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setValue(event.target.value);
+                }}
+              />
+              <ErrorMessage name="value" render={renderErrorMessage}/>
+              <Field
+                placeholder="Foto do produto"
+                name="image"
+                type="number"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setImage(event.target.value);
+                }}
+              />
+              <ErrorMessage name="image" render={renderErrorMessage}/>
+              <Field
+                placeholder="Categoria do produto"
+                name="category"
+                onChange={(event) => {
+                  formik.handleChange(event);
+                  setCategory(event.target.value);
+                }}
+              />
+              <ErrorMessage name="category" render={renderErrorMessage}/>
+              <button type="submit">Adicionar produto</button>
+            </Form>
+          )}
+        </Formik>
       </InputBoxContainer>
     </>
   );
 }
 
 const InputBoxContainer = styled.div`
-
-@media (max-width:700px){
+  @media (max-width: 700px) {
     width: 100%;
-}
-  form {
+  }
+  Form {
     display: flex;
     flex-direction: column;
-    @media (max-width:700px){
-    width: 100%;
-}
+    @media (max-width: 700px) {
+      width: 100%;
+    }
   }
   input {
-    margin-bottom: 10px;
     padding: 20px;
     border-radius: 5px;
     font-size: 20px;
-    @media (max-width:700px){
-    width: 100%;
-}
+    @media (max-width: 700px) {
+      width: 100%;
+    }
   }
   button {
     background-color: #1877f2;
@@ -149,21 +213,21 @@ const InputBoxContainer = styled.div`
     cursor: pointer;
     border: none;
     font-size: 20px;
-    @media (max-width:700px){
-    width: 100%;
-}
+    @media (max-width: 700px) {
+      width: 100%;
+    }
   }
-  .arrowBack{
+  .arrowBack {
     color: white;
-    font-size: 25px ;
+    font-size: 25px;
     position: fixed;
     left: 100px;
     cursor: pointer;
-    @media (max-width:700px){
-    top: 30px;
-    left: 30px;
-    z-index:1000000000;
-}
+    @media (max-width: 700px) {
+      top: 30px;
+      left: 30px;
+      z-index: 1000000000;
+    }
   }
   width: 100%;
   height: 100%;
